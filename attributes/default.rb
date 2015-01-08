1be140760363c90c72e62dd641d319db6e683a0c
Autumn Wang <autumn.wang@autodesk.com>
2015-01-08 09:11:22 -0800

    Added encrypted data bag logic

--- attributes/default.rb
+++ attributes/default.rb
@@ -16,7 +16,18 @@ default['uchiwa']['manage_service'] = true
 
 # Uchiwa Settings
 default['uchiwa']['settings']['user'] = 'admin'
-default['uchiwa']['settings']['pass'] = 'supersecret'
+
+databag_name = node['databag']['databag_name']
+if node['databag'].has_key?('databag_secret')
+  databag_secret = node['databag']['databag_secret']
+elsif node['databag'].has_key?('secret_url')
+  databag_secret = get_secret_from_url(node['databag']['secret_url'])
+end
+
+passwords = Chef::EncryptedDataBagItem.load(databag_name, 'passwords', secret=databag_secret)  
+default['uchiwa']['settings']['pass'] = passwords['uchiwa']
+#default['uchiwa']['settings']['pass'] = 'supersecret'
+
 default['uchiwa']['settings']['refresh'] = 5
 default['uchiwa']['settings']['host'] = '0.0.0.0'
 default['uchiwa']['settings']['port'] = 3000
@@ -32,3 +43,4 @@ default['uchiwa']['api'] = [
     'timeout' => 5
   }
 ]
+
